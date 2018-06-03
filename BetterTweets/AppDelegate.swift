acbdd0e52d83fb0ea3d892cf0b58e0e049b80fee
Colin Brevitz <verlander94@gmail.com>
2018-06-03 17:20:14 -0400

    Initial Commit

---
+++ BetterTweets/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BetterTweets
+//
+//  Created by Colin Brevitz on 6/3/18.
+//  Copyright © 2018 Colin Brevitz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
